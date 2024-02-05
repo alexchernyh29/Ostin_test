6516c4b71cc0de74b781d908ad08d32f4af25e00
Alexander Chernyh <alexchernyh2000@mail.ru>
2024-02-05 16:54:57 +0300

    finish

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,37 +15,53 @@ $(() => {
 			$(item)
 				.find(".test__text")
 				.html(questions[activeIndex].answers[id].text);
-    });
-	$(".test__counter span").html(activeIndex + 1);
-	$(".test__title").html(questions[activeIndex].title);
-}
+    	});
+		$(".test__counter span").html(activeIndex + 1);
+		$(".test__title").html(questions[activeIndex].title);
+	}
+
+	$('.test__item').on('click', function(){
+		$('.test__item.pink').removeClass('pink');
+		$(this).addClass('pink');
+	});
+
+	$('.test__item').hover(function() {
+		$(this).addClass('active-hover');
+	  }, function() {
+		$(this).removeClass('active-hover');
+	  });
 
 	$(".test").on("click", ".test__item", function (e) {
-		const id = $(e.target).closest(".test__item").data("id");
-		const isRightAnswer = questions[activeIndex].answers[id].isRightAnswer;
-		if (isRightAnswer) {
-			correctAnswers++;
-    	}
-		activeIndex += 1;;
-		if (activeIndex >= questions.length) {
-			$(".result").addClass("is-active");
-			$(".result__text").text("Вы правильно ответили на " + correctAnswers + " из " + questions.length + " вопросов");
-			if (correctAnswers <= 4){
-				$(".result__title").html(results[0].info);
-				$(".result__img").html(`<img src="${results[0].image}" />`);
-			} else {
-				$(".result__title").html(results[1].info);
-				$(".result__img").html(`<img src="${results[1].image}" />`);
+		setTimeout(() => {
+			$(".test__item").removeClass("pink");
+			$(".test__item").removeClass('active-hover');
+			const id = $(e.target).closest(".test__item").data("id");
+			const isRightAnswer = questions[activeIndex].answers[id].isRightAnswer;
+			if (isRightAnswer) {
+				correctAnswers++;
 			}
-			$(".result__button").on("click", function () {
-				$(".result").removeClass("is-active");
-				resetQuiz();
-			});
-		}
-		if (activeIndex >= questions.length) {
-			activeIndex = 0;
-		}
-		showQuestion();
+			activeIndex += 1;;
+			if (activeIndex >= questions.length) {
+				$(".result").addClass("is-active");
+				$(".result__text").text("Вы правильно ответили на " + correctAnswers + " из " + questions.length + " вопросов");
+				if (correctAnswers <= 4){
+					$(".result__title").html(results[0].info);
+					$(".result__img").html(`<img src="${results[0].image}" />`);
+				} else {
+					$(".result__title").html(results[1].info);
+					$(".result__img").html(`<img src="${results[1].image}" />`);
+				}
+				$(".result__button").on("click", function () {
+					$(".result").removeClass("is-active");
+					resetQuiz();
+				});
+			}
+			if (activeIndex >= questions.length) {
+				activeIndex = 0;
+			}
+			showQuestion();
+		}, 1000)
+		
 	});
 
 	function resetQuiz() {
